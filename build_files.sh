c631f91e78376c3b2ee91d0c0d60c0fb466993e1
Sayak Saha <83216382+sayakongit@users.noreply.github.com>
2023-04-23 03:51:53 +0530

    updated settings

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,4 @@
 echo "BUILD START"
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
-python manage.py migrate
-python manage.py create_employees
 echo "BUILD END"
--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-)l=-_cvji0!0w9h7x)vqdpoc+%q2%&1)gnoixlvh(-#or2ncqc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', "127.0.0.1"]
+ALLOWED_HOSTS = ['localhost', "127.0.0.1", ".vercel.app"]
 
 LOGGING_CONFIG = None
 # Application definition
